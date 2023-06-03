08c5d3c71256e27b43f3e5bf106a122db1fcae21
Your Name <you@example.com>
2023-06-03 16:55:55 +0200

    dialog showing and hiding plus basic style

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+console.log('Hello World');
+const createButton = document.querySelector('#createButton');
+const taskDialog = document.querySelector('#taskDialog');
+const confirmButton = taskDialog.querySelector('#confirmButton');
+const cancelButton = taskDialog.querySelector('#cancelButton');
+
+// "Show the dialog" button opens the <dialog> modally
+createButton.addEventListener('click', () => {
+  taskDialog.show();
+  console.log('show dialog');
+});
+
+// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
+confirmButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('close dialog');
+  taskDialog.close(); // Have to send the select box value here.
+});
