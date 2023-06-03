a543f03040d7a2fd8123baa27823c79472971d6a
Your Name <you@example.com>
2023-06-03 15:07:47 +0200

    setup basic dialog element for creation

--- source/public/scripts/themeToggler.js
+++ source/public/scripts/themeToggler.js
@@ -1,9 +1,9 @@
-const themeButton = document.getElementById("theme-button");
-themeButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
-  if(themeButton.innerText === '☽'){
+const themeButton = document.getElementById('themeButton');
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+  if (themeButton.innerText === '☽') {
     themeButton.innerText = '☼';
   } else {
     themeButton.innerText = '☽';
-  };
+  }
 });
