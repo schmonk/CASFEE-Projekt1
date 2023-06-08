e5dabac9145a3a1ca891cace0719153e879bfbab
Your Name <you@example.com>
2023-06-08 15:58:23 +0200

    lint indent removed, style main page. add reset css as separate file

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,14 +1,12 @@
-console.log('Hello World');
-const createButton = document.querySelector('#createTaskButton');
+const createTaskButton = document.querySelector('#createTaskButton');
 const taskDialog = document.querySelector('#taskDialog');
 const confirmButton = taskDialog.querySelector('#createDialogButton');
 const cancelButton = document.querySelector('#cancelButton');
 // const cancelButton = taskDialog.querySelector('#cancelButton');
 
 // "Show the dialog" button opens the <dialog> modally
-createButton.addEventListener('click', () => {
+createTaskButton.addEventListener('click', () => {
   taskDialog.showModal();
-  console.log('show dialog');
 });
 
 // Prevent the "confirm" button from the default behavior of submitting the form,
---
+++ source/public/scripts/taskManager.js
@@ -0,0 +1,19 @@
+function renderTasks() {
+  // render;
+}
+
+function createTask() {
+  // create;
+}
+
+function updateTask() {
+  // update;
+}
+
+function deleteTask() {
+  // delete;
+}
+createTask();
+updateTask();
+deleteTask();
+renderTasks();
