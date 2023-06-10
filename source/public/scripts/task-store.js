d361d2fc898fda47228add82698f0c96cd925a43
Your Name <you@example.com>
2023-06-10 11:13:45 +0200

    add task storage file

---
+++ source/public/scripts/task-store.js
@@ -0,0 +1,10 @@
+const tasks = [
+  {
+    id: '01', title: 'Task 1', description: 'This is a description', importance: 3, dueDate: '2023-06-10', creationDate: '2023-06-10', completion: false,
+  },
+  {
+    id: '02', title: 'Task 2', description: 'This is a description', importance: 1, dueDate: '2023-06-10', creationDate: '2023-06-10', completion: true,
+  },
+];
+
+console.log(tasks.length);
