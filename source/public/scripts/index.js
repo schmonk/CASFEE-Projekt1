a5c30cfbdddf0b68d645ed03312a097b782049af
Your Name <you@example.com>
2023-06-11 13:29:26 +0200

    localStorage store set up. working on updating task

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,4 +1,4 @@
-import taskStore from './task-store.js';
+// import { tasks } from './tasks.json'; // gives disallowed MIME type error
 
 const createTaskButton = document.querySelector('#createTaskButton');
 const taskDialog = document.querySelector('#taskDialog');
@@ -10,25 +10,16 @@ const taskTitle = document.querySelector('#taskTitle');
 const taskImportance = document.querySelector('#taskImportance');
 const taskDueDate = document.querySelector('#taskDueDate');
 const taskDescription = document.querySelector('#taskDescription');
-// const taskCompletion = document.querySelector('#taskCompletion');
-
-/* const myNewTask = {
-  id: '42', title: 'Task 42', description: 'This is a description', importance: 1,
-  dueDate: '2023-06-10', creationDate: '2023-06-10', completion: true,
-}; */
-
-function addATask() {
-  // console.log('add task');
-  // taskStore.addTask(myNewTask);
-}
 
 function editTask(event) {
   const element = event.target.parentElement.parentElement; // the task container
-  taskTitle.value = element.querySelector('.task-title').textContent;
-  taskImportance.value = element.querySelector('.task-importance').textContent;
-  taskDescription.value = element.querySelector('.task-description').textContent;
+  const retrievedTask = localStorage.getItem(element.id.toString());
+  const parsedTask = JSON.parse(retrievedTask);
+  taskTitle.value = parsedTask.title;
+  taskImportance.value = parsedTask.importance;
+  taskDescription.value = parsedTask.description;
   taskDialog.querySelector('h2').textContent = 'Edit this task';
-  taskDialog.querySelector('#createDialogButton').textContent = 'Save';
+  taskDialog.querySelector('#createDialogButton').textContent = 'Update';
   taskDialog.showModal();
 }
 
@@ -44,6 +35,7 @@ function deleteTask(event) {
   if (event.target.classList.contains('task-delete')) {
     event.preventDefault();
     const element = event.target.parentElement.parentElement;
+    localStorage.removeItem(element.id.toString());
     element.remove();
   }
 }
@@ -60,30 +52,47 @@ function clearDialog() {
   taskImportance.value = '';
 }
 
+function createId() {
+  return Date.now().toString();
+}
+
 function createTask() {
+  const id = createId();
   const title = (taskTitle.value !== '') ? taskTitle.value : 'My Task Title';
   const dueDate = (taskDueDate.value !== '') ? taskDueDate.value : 'today';
   const description = (taskDescription.value !== '') ? taskDescription.value : 'No description';
   let importance = (taskImportance.value !== '') ? taskImportance.value : '3';
   importance = importance < 0 ? 0 : importance;
   importance = importance > 5 ? 5 : importance;
+  const newTask = {
+    id,
+    title,
+    dueDate,
+    description,
+    importance,
+  };
   taskList.insertAdjacentHTML(
     'beforeend',
-    `<article class="task-container">
+    `<article id="${newTask.id}" class="task-container">
       <button class="btn task-complete" >Complete</button>
       <div class="task-content">
-        <h3 class="task-title">${title}</h3>
-        <p class="task-description">${description}</p>
+        <h3 class="task-title">${newTask.title}</h3>
+        <p class="task-description">${newTask.description}</p>
       </div>
-      <p class="task-due-date">Due ${dueDate}</p>
-      <p class="task-created-date" >Created ${dueDate}</p>
-      <p >Importance: <span class="task-importance">${importance}</span></p>
+      <p class="task-due-date">Due ${newTask.dueDate}</p>
+      <p class="task-created-date" >Created ${newTask.dueDate}</p>
+      <p >Importance: <span class="task-importance">${newTask.importance}</span></p>
       <div class="buttongroup">
         <button class="btn task-delete">Delete</button>
         <button class="btn task-edit">Edit</button>
       </div>
     </article>`,
   );
+  localStorage.setItem(`${id}`, JSON.stringify(newTask));
+}
+
+function updateTask() {
+
 }
 
 document.addEventListener('click', (event) => {
@@ -114,11 +123,9 @@ createTaskButton.addEventListener('click', () => {
 confirmButton.addEventListener('click', (e) => {
   e.preventDefault();
   createTask();
-  addATask();
   taskDialog.close();
   clearDialog();
 });
-
 
 cancelButton.addEventListener('click', (e) => {
   e.preventDefault();
