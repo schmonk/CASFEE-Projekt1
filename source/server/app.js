193583244dcd05b23258090036ac7b7fbb741270
Your Name <you@example.com>
2023-06-21 12:46:21 +0200

    restructured server files

--- source/server/app.js
+++ source/server/app.js
@@ -13,6 +13,27 @@ app.get('/', function (req, res) {
   // res.send('Hello GET');
 })
 
+
+// Retrieve all todos
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+// Add a new todo
+app.post('/todos', (req, res) => {
+  const { todo } = req.body;
+  todos.push(todo);
+  res.sendStatus(201);
+});
+
+// Delete a todo
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  todos = todos.filter((_, index) => index !== parseInt(id));
+  res.sendStatus(200);
+});
+
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening at http://localhost:${port}`);
