c110143c38d87de96696ed990d91073614c5bba6
Your Name <you@example.com>
2023-06-21 10:24:25 +0200

    install bodyParser, additional setup

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('source/client'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  console.log(`Hello woorldde`);
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+  console.log("Got a GET request for the homepage");
+  res.send('Hello GET');
+  res.render('/source/client/index.html');
+})
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+  console.log("Got a POST request for the homepage");
+  res.send('Hello POST');
+})
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+  console.log("Got a DELETE request for /del_user");
+  res.send('Hello DELETE');
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+  console.log("Got a GET request for /list_user");
+  res.send('Page Listing');
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {   
+  console.log("Got a GET request for /ab*cd");
+  res.send('Page Pattern Match');
 })
 
 app.listen(port, () => {
